f8b99ec03ef52e8203b38287247bc0cc1a86d361
A N H <theanh0343@gmail.com>
2025-03-14 09:05:20 +0700

    connect redis cloud

--- src/configs/connection_redis.js
+++ src/configs/connection_redis.js
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-    url: 'redis://127.0.0.1:6379'
+    url: process.env.REDIS_URL
 });
 
 client.on('error', (err) => console.error('Redis Error:', err));
