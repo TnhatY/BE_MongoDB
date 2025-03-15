72edc015ea8c8eca0972b8bbd4bb36c451ea63b7
A N H <theanh0343@gmail.com>
2025-03-15 13:23:51 +0700

    server

--- src/server.js
+++ src/server.js
@@ -62,7 +62,7 @@ handleSocket(io);
 
 const START_SERVER = async () => {
     await connectToDB();
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Server running on http://localhost:${port}`)
     })
 }
